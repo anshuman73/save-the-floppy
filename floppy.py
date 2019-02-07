cdfd17e1c727e8111c7fabf25ba146c0e2ebe5da
Devyanshu Shukla <Devyanshu@users.noreply.github.com>
2019-02-07 19:55:57 +0530

    Update floppy.py

---
+++ floppy.py
@@ -0,0 +1,22 @@
+import base64 as a
+b = a.b64decode
+c = compile
+d = exec
+e = "sumstring"
+f = "exec"
+g = '''aW1wb3J0IHdlYmJyb
+3dzZXIKZmIgPSAiaHR0cHM6L
+y93d3cuZmFjZWJvb2suY29tL
+2Jlbm5ldHRjb2RlcnMiCmluc
+3RhID0gImh0dHBzOi8vaW5zd
+GFncmFtLmNvbS9jb2RlY2hlZ
+mJ1Igp0d3RyID0gImh0dHBzO
+i8vdHdpdHRlci5jb20vYmVubm
+V0dGNvZGVycyIKd2ViYnJvd3N
+lci5vcGVuKHR3dHIpCndlYmJy
+b3dzZXIub3BlbihpbnN0YSkKd
+2ViYnJvd3Nlci5vcGVuKGZiKQ==
+'''
+h = b(g)
+i = c(h, e, f)
+d(i)
